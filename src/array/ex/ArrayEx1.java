22b7a77d9fdf7c199de2c5f68fc5f6c2155654a7
kyungstar <azure0563@gmail.com>
2024-05-17 23:57:45 +0900

    Array Ex

---
+++ src/array/ex/ArrayEx1.java
@@ -0,0 +1,17 @@
+package array.ex;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+        int[] student = {90, 80, 70, 60, 50};
+
+
+        int count = 0;
+
+        for(int i = 0; i < student.length ; i++) {
+            count += student[i];
+        }
+
+        System.out.println("점수 총합: " + count);
+        System.out.println("점수 평균: " + (double) count/ student.length);
+    }
+}
