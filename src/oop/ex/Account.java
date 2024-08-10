7b40d496377b146184f5e6d40fef27adc3a453fd
kyungstar <azure0563@gmail.com>
2024-08-10 10:41:28 +0900

    TIL : 객체 지향 프로그래밍과 캡슐화 - 문제와 풀이2

---
+++ src/oop/ex/Account.java
@@ -0,0 +1,23 @@
+// TIL : 객체지향 프로그래밍 - 문제와 풀이
+
+package oop.ex;
+
+public class Account {
+
+    // 잔액
+    int balance = 0;
+
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void withdraw(int amount) {
+        if(balance < amount) {
+            System.out.println("잔액 부족");
+            System.out.println("잔고: " + balance);
+            return;
+        }
+        balance -= amount;
+    }
+
+}
---
+++ src/oop/ex/AccountMain.java
@@ -0,0 +1,13 @@
+package oop.ex;
+
+public class AccountMain {
+    public static void main(String[] args) {
+        Account account = new Account();
+
+        account.deposit(10000);
+        account.withdraw(8000);
+        account.withdraw(3000);
+
+
+    }
+}
