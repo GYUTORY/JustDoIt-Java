36c5e7dfd4a401bc8c010a2c178235aeb8989d7a
kyungstar <azure0563@gmail.com>
2024-08-15 15:00:55 +0900

    TIL : 패키지 import

---
+++ src/pack/PackageMain2.java
@@ -0,0 +1,12 @@
+// TIL - 패키지 - import
+
+package pack;
+
+import pack.a.*;
+
+public class PackageMain2 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User uaer = new User();
+    }
+}
