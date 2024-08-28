d032c321ca80fea5321fcaf3f52c56ebff9f1e54
kyungstar <azure0563@gmail.com>
2024-08-28 22:08:16 +0900

    TIL : 스택 영역과 힙 영역 1

---
+++ src/memory/Data.java
@@ -0,0 +1,15 @@
+// TIL - 스택 영역과 힙 영역
+
+package memory;
+
+public class Data {
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
