8d6caf2869b93dc4c3a9136ce356626372f66428
kyungstar <azure0563@gmail.com>
2024-09-05 22:37:00 +0900

    TIl - statics 변수 2 - 1

---
+++ src/static1/Data3.java
@@ -0,0 +1,13 @@
+// TIl - statics 변수 2
+
+package static1;
+
+public class Data3 {
+    public String name;
+    public static int count;
+
+    public Data3(String name) {
+        this.name = name;
+        count++;
+    }
+}
