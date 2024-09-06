f0c7ca604ae739b5718242549100c10f61fe81f6
kyungstar <azure0563@gmail.com>
2024-09-06 12:24:30 +0900

    TIl - statics 변수 2 - 4

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -16,6 +16,13 @@ public class DataCountMain3 {
         Data3 data3 = new Data3("C");
         System.out.println("C count=" + Data3.count);
 
+        // 인스턴스를 통한 접근
+        // -> 가져다 쓰는 입장에서, 추천되지 않음. 개발자에게 혼동의 여지를 발생시킨다.
+        Data3 data4 = new Data3("D");
+        System.out.println("D count=" + data4.count);
+
+        // 클래스를 통한 접근
+        System.out.println("D count=" + Data3.count);
 
     }
 }
