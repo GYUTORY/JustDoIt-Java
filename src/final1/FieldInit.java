9ae7d561028899d3c1c6e30a5c3bfddcc52acda5
kyungstar <azure0563@gmail.com>
2024-09-09 21:43:48 +0900

    TIL - final 변수와 참조

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,16 @@
+// TIL - final 변수와 상수 1
+
+package final1;
+
+public class FieldInit {
+
+
+    // 변하지 않는, 공용 변수
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    // 이미 할당 된 경우, 생성자로도 불가능하다.
+    /*public FieldInit(int value) {
+        this.value = value;
+    }*/
+}
