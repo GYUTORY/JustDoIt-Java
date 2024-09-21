41b1ce99dacfa223b95f08090b9e7b23d33c77f6
kyungstar <azure0563@gmail.com>
2024-09-21 23:09:23 +0900

    TIL) super - 메인에서 실행과정

--- src/extends1/super2/Super2Main.java
+++ src/extends1/super2/Super2Main.java
@@ -2,6 +2,9 @@ package extends1.super2;
 
 public class Super2Main {
     public static void main(String[] args) {
+        // ClassA -> ClassB -> ClassC의 순서로 실행된다.
+        // 초기화는 최상위 부모부터 이루어진다.
+        // -> 자식 생성자의 첫줄에서 부모 생성자를 호출해야 하기 때문이다.
         ClassC classC = new ClassC();
     }
 }
