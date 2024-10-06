b46c1675c201b97578abaff33ee03db70905789a
kyungstar <azure0563@gmail.com>
2024-10-07 05:39:15 +0900

    TIL - 인터페이스 1

---
+++ src/poly/ex5/Cat.java
@@ -0,0 +1,17 @@
+// TIL - 인터페이스
+
+package poly.ex5;
+
+// implements : 구현
+// extends : 상속
+public class Cat implements InterfaceAnimal {
+    @Override
+    public void sound() {
+        System.out.println("야옹");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("고양이 이동");
+    }
+}
