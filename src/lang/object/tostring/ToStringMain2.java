d573ca7edeb16a61570d15f9bfa0f40bf42346b3
kyungstar <azure0563@gmail.com>
2024-10-17 21:01:39 +0900

    TIL) Java - Object(toString())

---
+++ src/lang/object/tostring/ToStringMain2.java
@@ -0,0 +1,29 @@
+// TIL - Object toString()
+
+package lang.object.tostring;
+
+public class ToStringMain2 {
+    public static void main(String[] args) {
+        Car car = new Car("Model Y");
+        Dog dog1 = new Dog("멍멍이1", 2);
+        Dog dog2 = new Dog("멍멍이2", 5);
+
+        System.out.println("1. 단순 toString 출력");
+        System.out.println(car.toString());
+        System.out.println(dog1.toString());
+        System.out.println(dog2.toString());
+
+        System.out.println("2. printLn 내부에서 toString 호출");
+        System.out.println(car);
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        System.out.println("3. printLn 내부에서 toString 호출");
+        ObjectPrinter.print(car);
+        ObjectPrinter.print(dog1);
+        ObjectPrinter.print(dog2);
+
+        String refValue = Integer.toHexString(System.identityHashCode(dog1));
+        System.out.println("refValue = " + refValue);
+    }
+}
