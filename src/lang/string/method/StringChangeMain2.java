c76561d9e972e5db96bba148d5a45da3aacac494
kyungstar <azure0563@gmail.com>
2024-11-09 11:48:17 +0900

    TIL - String 주요메서드 2 - 1

---
+++ src/lang/string/method/StringChangeMain2.java
@@ -0,0 +1,14 @@
+package lang.string.method;
+
+public class StringChangeMain2 {
+
+    public static void main(String[] args) {
+        String stwWithSpaces = "     Java Promgramming ";
+
+        System.out.println("소문자로 변환: " + stwWithSpaces.toLowerCase());
+        System.out.println("대문자로 변환: " + stwWithSpaces.toUpperCase());
+
+        System.out.println("공백 제거(trim) : '" + stwWithSpaces.trim() + "'");
+        System.out.println("공백 제거(strip) : '" + stwWithSpaces.strip() + "'");
+    }
+}
