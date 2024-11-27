6b9bc919b85fc6127d5f345eb144c809d2085160
kyungstar <azure0563@gmail.com>
2024-11-27 22:00:55 +0900

    Set.

---
+++ src/lang/clazz/ClassCreateMain.java
@@ -0,0 +1,15 @@
+package lang.clazz;
+
+public class ClassCreateMain {
+    public static void main(String[] args) throws Exception {
+        Class helloClass = Hello.class;
+        // Class helloClass = helloClass.forName("lang.clazz.Hello");
+
+        // getDeclaredConstructor() -> 생성자를 선택
+        Hello hello = (Hello) helloClass.getDeclaredConstructor().newInstance();
+        String result = hello.hello();
+        System.out.println("result = " + result);
+
+
+    }
+}
