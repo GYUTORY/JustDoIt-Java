bb6cce5c2559e26607d9124e8457ce2f60d71c99
kyungstar <azure0563@gmail.com>
2025-04-25 21:30:19 +0900

    Java_Mid - Clean

--- src/Java_Mid/clazz/Hello.java
+++
@@ -1,7 +0,0 @@
-package Java_Mid.clazz;
-
-public class Hello {
-    public String hello() {
-        return "hello";
-    }
-}
--- src/Java_Mid/enumeration/ref2/DiscountService.java
+++
@@ -1,9 +0,0 @@
-// #열거형이 만들어진 이유
-package Java_Mid.enumeration.ref2;
-
-
-public class DiscountService {
-    public int discount(Grade grade, int price) {
-        return price * grade.getDiscountPercent() / 100;
-    }
-}
